0c66bbc48d4d98f7619da44b886211f3899a35ef
Chen <i@cuichen.cc>
2023-10-16 23:38:06 +0800

    fix hm

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
--- home-manager/programs/zsh.nix
+++ home-manager/programs/zsh.nix
@@ -2,7 +2,7 @@
   programs.zsh = {
     enable = true;
     enableAutosuggestions = true;
-    enableSyntaxHighlighting = true;
+    syntaxHighlighting.enable = true;
     historySubstringSearch.enable = true;
 
     oh-my-zsh = {
