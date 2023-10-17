51ee16a252754165c168322ee8a1d7ca81fef9e5
Chen <i@cuichen.cc>
2023-10-17 11:02:18 +0800

    update fonts

--- home-manager/fonts.nix
+++ home-manager/fonts.nix
@@ -7,11 +7,36 @@
     fira-code
     fira-code-symbols
     font-awesome
+    hack-font
+    jetbrains-mono
     nerdfonts
+    noto-fonts
     source-code-pro
+    source-han-sans
+    source-han-serif
     wqy_microhei
     wqy_zenhei
   ];
 
-  fonts.fontconfig.enable = true;
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      emoji = [ "Noto Color Emoji" ];
+      monospace = [
+        "Noto Sans Mono CJK SC"
+        "Sarasa Mono SC"
+        "DejaVu Sans Mono"
+      ];
+      sansSerif = [
+        "Noto Sans CJK SC"
+        "Source Han Sans SC"
+        "DejaVu Sans"
+      ];
+      serif = [
+        "Noto Serif CJK SC"
+        "Source Han Serif SC"
+        "DejaVu Serif"
+      ];
+    };
+  };
 }
