7a5440bb35b59bd8a0403a3d189c166189993182
Chen <i@cuichen.cc>
2023-10-17 08:08:36 +0800

    gnome dconf

---
+++ home-manager/gnome.nix
@@ -0,0 +1,11 @@
+{
+  dconf.settings = {
+    "org/gnome/shell" = {
+      favorite-apps = [
+        "chromium.desktop"
+        "code.desktop"
+        "org.gnome.Terminal.desktop"
+      ];
+    };
+  };
+}
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -7,6 +7,7 @@
   ...
 }: {
   imports = [
+    ./gnome.nix
     ./programs
     ./packages.nix
   ];
