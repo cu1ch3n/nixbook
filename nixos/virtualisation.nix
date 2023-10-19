eed2b01344ee6f31d7da6455f058dd5b1dcec9ab
Chen <i@cuichen.cc>
2023-10-19 14:00:15 +0800

    fix virtualisation

--- nixos/virtualisation.nix
+++ nixos/virtualisation.nix
@@ -16,15 +16,15 @@
       swtpm.enable = true;
       runAsRoot = false;
     };
+  };
 
-    environment.etc = {
-      "ovmf/edk2-x86_64-secure-code.fd" = {
-        source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-x86_64-secure-code.fd";
-      };
+  environment.etc = {
+    "ovmf/edk2-x86_64-secure-code.fd" = {
+      source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-x86_64-secure-code.fd";
+    };
 
-      "ovmf/edk2-i386-vars.fd" = {
-        source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-i386-vars.fd";
-      };
+    "ovmf/edk2-i386-vars.fd" = {
+      source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-i386-vars.fd";
     };
   };
 }
