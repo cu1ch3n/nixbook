e8a26975cd4894a2f366bfec46712c1c9055c966
Chen <i@cuichen.cc>
2023-11-03 17:35:20 +0800

    steam

--- nixos/user.nix
+++ nixos/user.nix
@@ -8,4 +8,10 @@
   };
 
   programs.zsh.enable = true;
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
 }
