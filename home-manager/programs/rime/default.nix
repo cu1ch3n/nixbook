f074deb4484967ad936dc85d546d4ef45f4035c8
Chen <i@cuichen.cc>
2023-12-04 17:47:32 +0800

    .config/fcitx5/profile

--- home-manager/programs/rime/default.nix
+++ home-manager/programs/rime/default.nix
@@ -9,4 +9,24 @@ in
       recursive = true;
     };
   };
+
+  home.file = {
+    ".config/fcitx5/profile".text = ''
+      [Groups/0]
+      Name=Default
+      Default Layout=us
+      DefaultIM=rime
+
+      [Groups/0/Items/0]
+      Name=keyboard-us
+      Layout=
+
+      [Groups/0/Items/1]
+      Name=rime
+      Layout=
+
+      [GroupOrder]
+      0=Default
+    '';
+  };
 }
