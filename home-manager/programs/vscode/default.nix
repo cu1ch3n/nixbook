4ebb4c75b692fa8d9cdc88582005db0199512cbf
Chen <i@cuichen.cc>
2024-04-12 22:03:45 +0800

    format

--- home-manager/programs/vscode/default.nix
+++ home-manager/programs/vscode/default.nix
@@ -2,22 +2,21 @@
   programs.vscode = {
     enable = true;
     mutableExtensionsDir = true;
-    extensions = with pkgs.vscode-marketplace;
-      [
-        akamud.vscode-theme-onedark
-        akamud.vscode-theme-onelight
-        github.copilot
-        eamodio.gitlens
-        haskell.haskell
-        justusadam.language-haskell
-        yellpika.latex-input
-        jnoortheen.nix-ide
-        richie5um2.vscode-sort-json
-        vscode-icons-team.vscode-icons
-        coq-community.vscoq1
-        github.vscode-pull-request-github
-        github.copilot-chat
-      ];
+    extensions = with pkgs.vscode-marketplace; [
+      akamud.vscode-theme-onedark
+      akamud.vscode-theme-onelight
+      github.copilot
+      eamodio.gitlens
+      haskell.haskell
+      justusadam.language-haskell
+      yellpika.latex-input
+      jnoortheen.nix-ide
+      richie5um2.vscode-sort-json
+      vscode-icons-team.vscode-icons
+      coq-community.vscoq1
+      github.vscode-pull-request-github
+      github.copilot-chat
+    ];
     userSettings = import ./user-settings.nix;
   };
 }
