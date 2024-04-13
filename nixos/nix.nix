37a8530b66e8846f23d533305d643b41e150d2e5
Chen <i@cuichen.cc>
2024-04-13 16:37:32 +0800

    remove hyprland cachix

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -5,8 +5,6 @@
 }: {
   nix = {
     settings = {
-      substituters = [ "https://hyprland.cachix.org" ];
-      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
       experimental-features = "nix-command flakes";
       auto-optimise-store = true;
     };
