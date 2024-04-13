c1287e097d218f26347a0c8d0e05ff5bb2b1d62d
Chen <i@cuichen.cc>
2024-04-13 13:50:56 +0800

    workspace shortcuts

--- home-manager/programs/gnome/default.nix
+++ home-manager/programs/gnome/default.nix
@@ -114,6 +114,16 @@
       switch-to-workspace-7 = [ "<Super>7" ];
       switch-to-workspace-8 = [ "<Super>8" ];
       switch-to-workspace-9 = [ "<Super>9" ];
+
+      move-to-workspace-1 = [ "<Super><Shift>1" ];
+      move-to-workspace-2 = [ "<Super><Shift>2" ];
+      move-to-workspace-3 = [ "<Super><Shift>3" ];
+      move-to-workspace-4 = [ "<Super><Shift>4" ];
+      move-to-workspace-5 = [ "<Super><Shift>5" ];
+      move-to-workspace-6 = [ "<Super><Shift>6" ];
+      move-to-workspace-7 = [ "<Super><Shift>7" ];
+      move-to-workspace-8 = [ "<Super><Shift>8" ];
+      move-to-workspace-9 = [ "<Super><Shift>9" ];
     };
 
     "org/gnome/desktop/wm/preferences" = {
