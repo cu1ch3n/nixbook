7123d843e29d11d41984d875349f6c4e2ed95973
Chen <i@cuichen.cc>
2024-04-15 21:45:55 +0800

    Update VSCode extensions in home-manager configuration

--- home-manager/programs/vscode/default.nix
+++ home-manager/programs/vscode/default.nix
@@ -2,22 +2,32 @@
   programs.vscode = {
     enable = true;
     mutableExtensionsDir = true;
-    extensions = with pkgs.vscode-marketplace; [
-      akamud.vscode-theme-onedark
-      akamud.vscode-theme-onelight
-      coq-community.vscoq1
-      eamodio.gitlens
-      github.copilot
-      github.copilot-chat
-      github.vscode-pull-request-github
-      haskell.haskell
-      jnoortheen.nix-ide
-      justusadam.language-haskell
-      richie5um2.vscode-sort-json
-      skellock.just
-      vscode-icons-team.vscode-icons
-      yellpika.latex-input
-    ];
+    extensions = with pkgs.vscode-marketplace;
+      [
+        akamud.vscode-theme-onedark
+        akamud.vscode-theme-onelight
+        coq-community.vscoq1
+        eamodio.gitlens
+        github.copilot
+        github.vscode-pull-request-github
+        haskell.haskell
+        jnoortheen.nix-ide
+        justusadam.language-haskell
+        mgt19937.typst-preview
+        nvarner.typst-lsp
+        richie5um2.vscode-sort-json
+        skellock.just
+        vscode-icons-team.vscode-icons
+        yellpika.latex-input
+      ]
+      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "copilot-chat";
+          publisher = "github";
+          version = "0.14.1";
+          sha256 = "d5oohDNF44+3FRYOIAv32hSgFvvggugDP+kbOAcYfX0=";
+        }
+      ];
     userSettings = import ./user-settings.nix;
   };
 }
