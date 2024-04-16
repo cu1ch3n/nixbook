57a773fecf61c5a4b46fd4bd7bb87a927f21c0b9
Chen <i@cuichen.cc>
2024-04-17 02:54:36 +0800

    add agenix and secrets directory

--- flake.nix
+++ flake.nix
@@ -9,6 +9,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     nur.url = "github:nix-community/NUR";
     hardware.url = "github:nixos/nixos-hardware";
     impermanence.url = "github:nix-community/impermanence";
@@ -36,7 +41,7 @@
     nixosConfigurations = {
       nixbook = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
-        modules = [./nixos/configuration.nix];
+        modules = [./nixos/configuration.nix ./secrets];
       };
     };
   };
---
+++ secrets/default.nix
@@ -0,0 +1,3 @@
+{inputs, ...}: {
+  imports = [inputs.agenix.nixosModules.default];
+}
