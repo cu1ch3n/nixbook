1c36edf1f5cf78a4accf6469e95b69d89fb5d377
Chen <i@cuichen.cc>
2024-04-17 19:18:34 +0800

    enable fprintd

--- nixos/hardware.nix
+++ nixos/hardware.nix
@@ -93,7 +93,7 @@
   };
 
   # Fingerprint
-  # services.fprintd.enable = true;
+  services.fprintd.enable = true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
