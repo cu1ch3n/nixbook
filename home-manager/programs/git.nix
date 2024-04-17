3aa7682951963ed27354b51c274acd9768c3f47d
Chen <i@cuichen.cc>
2024-04-17 18:39:57 +0800

    git signing key

--- home-manager/programs/git.nix
+++ home-manager/programs/git.nix
@@ -1,7 +1,15 @@
-{
+{pkgs, ...}: {
   programs.git = {
     enable = true;
     userName = "Chen";
     userEmail = "i@cuichen.cc";
+    extraConfig = {
+      user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAikGltB46LIhsjvVIa2X8iP2do5CnpVXJojvfGiYsmW";
+      gpg = {
+        format = "ssh";
+        "ssh".program = "${pkgs._1password-gui}/bin/op-ssh-sign";
+      };
+      commit.gpgsign = true;
+    };
   };
 }
