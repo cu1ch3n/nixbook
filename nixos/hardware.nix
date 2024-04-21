a532ddf7b3ab082d8b23a1aee4d2d41cacb9c45d
Chen <i@cuichen.cc>
2024-04-21 15:35:40 +0800

    Add swap device

--- nixos/hardware.nix
+++ nixos/hardware.nix
@@ -78,6 +78,7 @@
   };
 
   boot.tmp.cleanOnBoot = true;
+  swapDevices = [ { device = "/swap/swapfile"; } ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
