d28b2f83306d30a59969109279f6d930b720da19
Chen <i@cuichen.cc>
2024-04-28 22:34:34 +0800

    update openmeteo settings

--- nixos/services/homepage/widgets.nix
+++ nixos/services/homepage/widgets.nix
@@ -8,6 +8,16 @@
       };
     }
     {
+      openmeteo = {
+        label = "Hong Kong";
+        latitude = 22.2783;
+        longitude = 114.1747;
+        timezone = "Asia/Hong_Kong";
+        units = "metric";
+        cache = 5;
+      };
+    }
+    {
       search = {
         provider = "google";
         target = "_blank";
@@ -19,15 +29,6 @@
         provider = "custom";
         url = "https://search.nixos.org/packages?channel=unstable&query=";
         target = "_blank";
-      };
-    }
-    {
-      openmeteo = {
-        latitude = 22.2783;
-        longitude = 114.1747;
-        timezone = "Asia/Hong_Kong";
-        units = "metric";
-        cache = 5;
       };
     }
   ];
