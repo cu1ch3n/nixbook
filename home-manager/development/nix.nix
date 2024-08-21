e2a371f536cf001fd09bc13e7bd4db59ae6ee444
Cui Chen <i@cuichen.cc>
2024-08-22 01:01:52 +0800

    remove json2nix

--- home-manager/development/nix.nix
+++ home-manager/development/nix.nix
@@ -1,14 +1,6 @@
-{pkgs, ...}: let
-  json2nix = pkgs.writeScriptBin "json2nix" ''
-    ${pkgs.python3}/bin/python ${pkgs.fetchurl {
-      url = "https://gist.githubusercontent.com/Scoder12/0538252ed4b82d65e59115075369d34d/raw/e86d1d64d1373a497118beb1259dab149cea951d/json2nix.py";
-      hash = "sha256-ROUIrOrY9Mp1F3m+bVaT+m8ASh2Bgz8VrPyyrQf9UNQ=";
-    }} $@
-  '';
-in {
+{pkgs, ...}: {
   home.packages = with pkgs; [
     alejandra
-    json2nix
     comma
     deadnix
     nil
