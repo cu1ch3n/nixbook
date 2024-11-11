e75083f57fea737bde211839d906c54e8f053b80
Chen <i@cuichen.cc>
2024-11-11 20:12:14 +0800

    wechat no more need license

--- home-manager/programs/wechat/default.nix
+++ home-manager/programs/wechat/default.nix
@@ -1,10 +1,5 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    (wechat-uos.override {
-      uosLicense = pkgs.fetchurl {
-        url = "https://aur.archlinux.org/cgit/aur.git/plain/license.tar.gz?h=wechat-uos-bwrap";
-        hash = "sha256-U3YAecGltY8vo9Xv/h7TUjlZCyiIQdgSIp705VstvWk=";
-      };
-    })
+    wechat-uos
   ];
 }
