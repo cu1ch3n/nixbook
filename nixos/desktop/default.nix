37ff25146d9a5dcd1595568d957b4c2ab7104875
Chen <i@cuichen.cc>
2024-11-30 12:51:36 +0800

    switch back to gnome

--- nixos/desktop/default.nix
+++ nixos/desktop/default.nix
@@ -6,17 +6,19 @@
     xkb.layout = "us";
   };
   services.displayManager = {
-    sddm = {
-      enable = true;
-      wayland.enable = true;
-    };
-    defaultSession = "plasma";
+    # sddm = {
+    #   enable = true;
+    #   wayland.enable = true;
+    # };
+    # defaultSession = "plasma";
+    gdm.enable = true;
   };
-  services.desktopManager.plasma6.enable = true;
+  # services.desktopManager.plasma6.enable = true;
+  services.desktopManager.gnome.enable = true;
   services.libinput.enable = true;
 
   # For trash
-  # services.gvfs.enable = true;
+  services.gvfs.enable = true;
 
   virtualisation.waydroid.enable = true;
 
