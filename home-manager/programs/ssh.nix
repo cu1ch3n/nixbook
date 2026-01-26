7db1e06cd329968e63a3e9645163408168318b5d
Chen <i@cuichen.cc>
2026-01-26 12:16:35 +0800

    ssh: add github.com-bot host using bot key

--- home-manager/programs/ssh.nix
+++ home-manager/programs/ssh.nix
@@ -2,12 +2,24 @@
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
+
     matchBlocks = {
+      # Default GitHub (routes via ssh.github.com:443)
       "github.com" = {
         hostname = "ssh.github.com";
         port = 443;
         user = "git";
       };
+
+      # Bot identity for pushing as cu1ch3n-bot
+      "github.com-bot" = {
+        hostname = "ssh.github.com";
+        port = 443;
+        user = "git";
+        identityFile = "~/.ssh/id_ed25519_cu1ch3n_bot";
+        extraOptions.IdentitiesOnly = "yes";
+      };
+
       "*" = {
         extraOptions.IdentityAgent = "~/.1password/agent.sock";
       };
